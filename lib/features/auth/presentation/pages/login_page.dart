4b53d3c06a2c2d8b6872d82c8ef21864a0b819d3
yafet <yafetesfaye11@gmail.com>
2025-08-21 12:10:14 +0300

    feat(auth): add custom widget and refactor the login page

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -1,80 +1,288 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lissan_ai/features/auth/presentation/widgets/custom_text_field.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
+      body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Welcome Back!',
-                  style: GoogleFonts.inter(
-                    fontSize: 32,
-                    fontWeight: FontWeight.w800,
-                    fontStyle: FontStyle.normal,
-                  ),
-                ),
-              ],
-            ),
-            Text(
-              'Ready to continue your English journey?',
-              style: GoogleFonts.inter(fontWeight: FontWeight.w400),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'Sign in',
-              style: GoogleFonts.inter(
-                color: const Color(0xFF08CC2F),
-                fontWeight: FontWeight.w800,
-                fontSize: 24,
-              ),
-            ),
-            Text(
-              'Continue your path to English mastery!',
-              style: GoogleFonts.inter(
-                fontSize: 12,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              decoration: BoxDecoration(
-                color: const Color(0xFFFCFCFC),
-                border: BoxBorder.all(color: const Color(0xFF92FFB3)),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(
-                    width: 30,
-                    height: 30,
-                    child: Image.asset('assets/images/google.png'),
-                  ),
-                  const SizedBox(width: 5),
-                  Text(
-                    'Sign In with Google',
-                    style: GoogleFonts.inter(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 16,
-                      color: const Color(0xFF08B129),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Welcome Back!',
+                          style: GoogleFonts.inter(
+                            fontSize: 32,
+                            fontWeight: FontWeight.w800,
+                            fontStyle: FontStyle.normal,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 70,
+                          width: 70,
+                          child: Image.asset('assets/images/celebrate.jpg'),
+                        ),
+                      ],
                     ),
-                  ),
+                    Text(
+                      'Ready to continue your English journey?',
+                      style: GoogleFonts.inter(fontWeight: FontWeight.w400),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      'Sign in',
+                      style: GoogleFonts.inter(
+                        color: const Color(0xFF08CC2F),
+                        fontWeight: FontWeight.w800,
+                        fontSize: 24,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Continue your path to English mastery!',
+                          style: GoogleFonts.inter(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+
+                        const SizedBox(width: 3),
+                        SizedBox(
+                          height: 30,
+                          width: 20,
+                          child: Transform.rotate(
+                            angle: 22.92 * math.pi / 180,
+                            child: Image.asset('assets/images/rocket.jpg'),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 25),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFFCFCFC),
+                          border: BoxBorder.all(color: const Color(0xFF92FFB3)),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            SizedBox(
+                              width: 30,
+                              height: 30,
+                              child: Image.asset('assets/images/google.png'),
+                            ),
+                            const SizedBox(width: 5),
+                            Text(
+                              'Sign In with Google',
+                              style: GoogleFonts.inter(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                                color: const Color(0xFF08B129),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Row(
+                      children: [
+                        Expanded(
+                          child: Divider(thickness: 1, color: Colors.grey),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Text(
+                            'Or continue with email',
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ),
+                        Expanded(
+                          child: Divider(thickness: 1, color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Form(
+                      key: formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          CustomTextField(
+                            controller: emailController,
+                            title: 'Email Address',
+                            icon: Icons.email,
+                            labelText: 'Email',
+                          ),
+                          const SizedBox(height: 15),
+                          CustomTextField(
+                            controller: passwordController,
+                            title: 'Password',
+                            icon: Icons.password,
+                            labelText: 'password',
+                            obscure: true,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 18),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Checkbox(value: false, onChanged: (value) {}),
+                            const Text('Remember Me'),
+                          ],
+                        ),
+                        Text(
+                          'Forgot Password?',
+                          style: GoogleFonts.inter(
+                            fontWeight: FontWeight.w600,
+                            color: const Color(0xFF08B129),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 18),
+                    Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        gradient: const LinearGradient(
+                          colors: [
+                            Color(0xFF6BDB54),
+                            Color(0xFF38A0AB),
+                            Color(0xFF1848F4),
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          shadowColor: Colors.transparent,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 24,
+                            vertical: 14,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: const Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Sign In',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(width: 8),
+                            Icon(
+                              Icons.arrow_forward,
+                              size: 20,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'New to LissanAI?',
+                          style: GoogleFonts.inter(
+                            fontWeight: FontWeight.w500,
+                            color: const Color(0xFF757575),
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        Text(
+                          'Create Account',
+                          style: GoogleFonts.inter(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            color: const Color(0xFF08B129),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(bottom: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _buildCard(Colors.green, 'Secure Login'),
+                  const SizedBox(width: 10),
+                  _buildCard(Colors.blue, 'Fast Access'),
+                  const SizedBox(width: 10),
+                  _buildCard(Colors.deepPurpleAccent, 'Always Available'),
                 ],
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildCard(Color color, String text) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          width: 12,
+          height: 12,
+          decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+        ),
+        const SizedBox(width: 8),
+        Text(text, style: GoogleFonts.inter(fontSize: 13)),
+      ],
     );
   }
 }
---
+++ lib/features/auth/presentation/widgets/custom_text_field.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomTextField extends StatefulWidget {
+  final String title;
+  final TextEditingController controller;
+  final IconData icon;
+  final String labelText;
+  final bool obscure;
+  const CustomTextField({
+    required this.controller,
+    required this.title,
+    required this.icon,
+    required this.labelText,
+    this.obscure = false,
+    super.key,
+  });
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  late bool _obscureText;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.obscure;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          widget.title,
+          style: GoogleFonts.inter(fontSize: 12, fontWeight: FontWeight.w600),
+        ),
+        const SizedBox(height: 4),
+        TextFormField(
+          obscureText: _obscureText,
+          controller: widget.controller,
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 5,
+            ),
+            prefixIcon: Icon(widget.icon, color: const Color(0xFFC9C9C9)),
+            labelText: widget.labelText,
+            labelStyle: const TextStyle(color: Color(0xFFC9C9C9)),
+            fillColor: const Color(0xFFFCFCFC),
+            filled: true,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Color(0xFFD9D8D8)),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Color(0xFFD9D8D8)),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Color(0xFFD9D8D8)),
+            ),
+
+            suffixIcon: widget.obscure
+                ? IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility_off : Icons.visibility,
+                      color: const Color(0xFFC9C9C9),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  )
+                : null,
+          ),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return '${widget.title} is required';
+            }
+            return null;
+          },
+        ),
+      ],
+    );
+  }
+}
