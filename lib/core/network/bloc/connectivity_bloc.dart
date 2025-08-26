8efae39c1b9f28a5c71509613fed64bcea89d1a6
yafet <yafetesfaye11@gmail.com>
2025-08-26 09:41:38 +0300

    feat(core) : add connectivity bloc for internet checking

---
+++ lib/core/network/bloc/connectivity_bloc.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+
+part 'connectivity_event.dart';
+part 'connectivity_state.dart';
+
+class ConnectivityBloc extends Bloc<ConnectivityEvent, ConnectivityState> {
+  late final StreamSubscription _subscription;
+
+  ConnectivityBloc() : super(ConnectivityInitial()) {
+    _subscription = InternetConnection().onStatusChange.listen((status) {
+      add(UpdateConnectivityEvent(status));
+    });
+
+    on<UpdateConnectivityEvent>((event, emit) {
+      if (event.status == InternetStatus.connected) {
+        emit(ConnectivityConnected());
+      } else {
+        emit(ConnectivityDisconnected());
+      }
+    });
+  }
+
+  @override
+  Future<void> close() {
+    _subscription.cancel();
+    return super.close();
+  }
+}
